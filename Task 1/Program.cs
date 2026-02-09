70572d91b97fa0685dd53c185a008c17e714fed0
zeyad <zeyad@LAPTOP-G93R4R6G>
2026-02-09 18:16:47 +0300

    Add project files.

---
+++ Task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("enter  bill amount : ");
+Double amount = Convert.ToDouble(Console.ReadLine());
+Double discont = 0;
+
+if (amount >= 500)
+{
+    discont = amount * 0.20;
+}
+else if (amount < 499 && amount >= 300)
+{
+    discont = amount * 0.10;
+}
+else
+{
+    discont = 0;
+
+}
+
+Double finalPrice = amount - discont;
+
+Console.WriteLine("this is final price : " + finalPrice);
